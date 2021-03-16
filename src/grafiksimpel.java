c36dfd0b6bdbf228e22411a9cb8e5a3526baf01b
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-03-16 10:59:24 +0100

    bas kåden för att endra på canvas

--- src/grafiksimpel.java
+++ src/grafiksimpel.java
@@ -1,2 +1,20 @@
-public class grafiksimpel {
+import javax.swing.*;
+import java.awt.*;
+
+public class grafiksimpel extends Canvas {
+    public grafiksimpel() {
+        JFrame frame = new JFrame("A simple painting");
+        this.setSize(600,600);
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+
+    }
+    public static void main(String[] args) {
+        grafiksimpel rityta = new grafiksimpel();
+    }
 }
