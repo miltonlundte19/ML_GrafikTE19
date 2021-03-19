a4b0034100bd83519c84f96dab7b802e4a98ba26
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-03-19 11:22:07 +0100

    her in figurerna (slut på lektion)

--- src/grafiksimpel.java
+++ src/grafiksimpel.java
@@ -14,9 +14,9 @@ public class grafiksimpel extends Canvas {
     public void paint(Graphics g) {
 
 
-        drawHouse(g, 100, 200);
+        drawHouse(g, 200, 300);
         drawTree(g,100,200);
-        drawStev(g,200,100);
+//        drawStev(g,200,100);
     }
 //   temperärt som stev för en simpel person.
     private void drawStev(Graphics g, int x, int y) {
@@ -24,12 +24,23 @@ public class grafiksimpel extends Canvas {
     }
 
     private void drawTree(Graphics g, int x, int y) {
+
+        g.setColor(new Color(163, 121, 27));
+        g.fillRect(13+x,17+y,7,13);
+
         g.setColor(Color.GREEN);
-        g.fillOval();
+        g.fillOval(x,y,34,20);
+
     }
 
     private void drawHouse(Graphics g, int x, int y) {
+        g.setColor(new Color(0xAA1111));
+        g.fillRect(x,y-40,50,40);
 
+        g.setColor(new Color(0x444444));
+        int[] xcoords = {x-5,x+25,x+55};
+        int[] ycoords = {y-40,y-65,y-40};
+        g.fillPolygon(xcoords,ycoords,3);
     }
 
     public static void main(String[] args) {
